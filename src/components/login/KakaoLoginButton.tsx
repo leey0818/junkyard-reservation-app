1e7a31fde0ddfa9ef3c012a8974d3e282a1ac13f
leey0818 <leey0818@gmail.com>
2024-08-09 21:01:06 +0900

    fix: 카카오 로그인 prefetch 비활성화

--- src/components/login/KakaoLoginButton.tsx
+++ src/components/login/KakaoLoginButton.tsx
@@ -5,6 +5,7 @@ export default function KakaoLoginButton() {
   return (
     <Link
       href="/login/sso/kakao"
+      prefetch={false}
       className="flex items-center justify-center select-none h-12 w-full min-w-48 max-w-96 px-4 rounded-md bg-[#FEE500] text-sm text-black/85 gap-2 cursor-pointer"
     >
       <KakaoSymbol />
