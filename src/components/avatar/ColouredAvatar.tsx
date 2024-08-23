b80ea8b7cd41debcf1e3ab5dbfc544c81dfdc261
leey0818 <leey0818@gmail.com>
2024-08-23 09:11:01 +0900

    fix: 아바타 이름 표시방식 개선
    - 한글이름 3-4자리 일 때, 이름부분이 표시되도록 개선

--- src/components/avatar/ColouredAvatar.tsx
+++ src/components/avatar/ColouredAvatar.tsx
@@ -6,6 +6,14 @@ type ColouredAvatarProps = {
   text: string;
 };
 
+const getShortenName = (text: string) => {
+  // 한글이름이면 뒷 2자리, 이 외에는 앞 2자리
+  if (/^[가-힣]{3,4}$/.test(text)) {
+    return text.slice(-2);
+  }
+  return text.slice(0, 2);
+};
+
 /**
  * 텍스트에 따라 결정되는 색상형 아바타
  * @param text 표시할 텍스트 (텍스트는 2자로 제한됨)
@@ -20,7 +28,7 @@ export default function ColouredAvatar({ text }: ColouredAvatarProps) {
         colour.isLight && 'text-white',
       )}
     >
-      {text.substring(0, 2)}
+      {getShortenName(text)}
     </AvatarWrapper>
   );
 };
