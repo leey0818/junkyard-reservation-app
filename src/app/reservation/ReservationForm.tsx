00241005c1a0e5e55e2c640508804dc627208afa
leey0818 <leey0818@gmail.com>
2024-08-30 16:51:15 +0900

    feat: 예약 API 연동

--- src/app/reservation/ReservationForm.tsx
+++ src/app/reservation/ReservationForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import FormInput from './FormInput';
 
 type ReservationFormProps = {
@@ -21,6 +21,7 @@ type FormValues = {
 };
 
 export default function ReservationForm(props: ReservationFormProps) {
+  const [loading, setLoading] = useState(false);
   const {
     register,
     formState: { errors },
@@ -28,9 +29,25 @@ export default function ReservationForm(props: ReservationFormProps) {
     handleSubmit,
   } = useForm<FormValues>();
 
-  const onFormSubmit: SubmitHandler<FormValues> = useCallback((formData) => {
-    console.log(formData);
-  }, []);
+  const onFormSubmit: SubmitHandler<FormValues> = useCallback(async (formData) => {
+    setLoading(true);
+    try {
+      const res = await fetch('/api/reservation/register', { method: 'POST', body: JSON.stringify(formData) });
+      if (res.ok) {
+        const result = await res.json();
+        if (result.success) {
+          alert('정상 예약되었습니다.');
+          props.onSubmit?.();
+        } else {
+          alert(`예약 실패: ${result.message}`);
+        }
+      } else {
+        alert(`서버와 통신 중 오류가 발생하였습니다. [${res.status}]`);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }, [props]);
 
   useEffect(() => {
     setValue('token', props.secToken);
@@ -41,42 +58,59 @@ export default function ReservationForm(props: ReservationFormProps) {
       <h1 className="font-semibold text-lg mb-8">예약하기</h1>
       <form onSubmit={handleSubmit(onFormSubmit)}>
         <FormInput
-          {...register('name', {required: true})}
+          {...register('name', { required: true, disabled: loading })}
           required={true}
           error={!!errors.name}
           label="성명"
           placeholder="성명을 입력하세요."
+          maxLength={20}
         />
         <FormInput
-          {...register('phoneNo', {required: true})}
+          {...register('phoneNo', {
+            required: true,
+            disabled: loading,
+            pattern: /^(01\d)-?(\d{3,4})-?(\d{4})$/,
+            onChange: (evt) => {
+              const value = evt.target.value.replace(/[^0-9]/g, '');
+              setValue('phoneNo', value.replace(/^(01\d)-?(\d{3,4})-?(\d{4})$/, '$1-$2-$3'), { shouldValidate: true });
+            },
+          })}
           required={true}
           error={!!errors.phoneNo}
           label="전화번호"
           placeholder="연락 가능한 전화번호를 입력하세요."
+          maxLength={14}
         />
         <FormInput
-          {...register('vendor', {required: true})}
+          {...register('vendor', { required: true, disabled: loading })}
           required={true}
           error={!!errors.vendor}
+          disabled={loading}
           label="제조사"
           placeholder="차량 제조사를 입력하세요. ex) 현대, 기아, 벤츠"
+          maxLength={10}
         />
         <FormInput
-          {...register('model', {required: true})}
+          {...register('model', { required: true, disabled: loading })}
           required={true}
           error={!!errors.model}
+          disabled={loading}
           label="모델명"
           placeholder="차량 모델명을 입력하세요. ex) 소나타, 그랜저"
+          maxLength={20}
         />
         <FormInput
-          {...register('carNo', {required: true})}
+          {...register('carNo', { required: true, disabled: loading })}
           required={true}
           error={!!errors.carNo}
+          disabled={loading}
           label="차량번호"
           placeholder="차량 번호를 입력하세요."
+          maxLength={9}
         />
         <FormInput
           {...register('note')}
+          disabled={loading}
           label="예약내용"
           placeholder="추가로 전달할 내용을 입력하세요."
         />
@@ -84,11 +118,13 @@ export default function ReservationForm(props: ReservationFormProps) {
         <button
           type="submit"
           className="w-full border border-blue-600 bg-blue-500 text-white rounded px-3 py-1 mt-4"
+          disabled={loading}
         >예약하기
         </button>
         <button
           type="button"
           className="w-full border border-gray-200 bg-gray-50 rounded mt-2 px-3 py-1"
+          disabled={loading}
           onClick={props.onCancel}
         >닫기
         </button>
