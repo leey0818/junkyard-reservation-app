7aee63ac8a15362d0985185ba7248f29445959c8
leey0818 <leey0818@gmail.com>
2024-10-03 21:38:43 +0900

    feat: 내 예약 화면 구현 및 목업 데이터 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { redirect } from 'next/navigation';
 import { cookies } from 'next/headers';
 import { doGET } from '@/backend/service';
@@ -27,6 +28,9 @@ type ReservationData = {
   }[];
 };
 
+const toPhoneNoFormat = (phoneNo: string) => phoneNo.replace(/^(01\d)-?(\d{3,4})-?(\d{4})$/, '$1-$2-$3');
+const toAmountFormat = (amount: number) => Number(amount).toLocaleString() + '원';
+
 export default async function Page() {
   const accessToken = cookies().get('accessToken')?.value;
   if (!accessToken) {
@@ -40,24 +44,48 @@ export default async function Page() {
     },
   });
 
-  console.log(result);
-
+  if (result.code !== 'NORMAL') {
+    console.log(JSON.stringify(result));
+    return (
+      <>
+        <AppHeader>내 예약</AppHeader>
+        <div className="p-6">
+          <p className="text-center">일시적으로 데이터를 가져올 수 없습니다.<br/>잠시 후 다시 시도해 주세요.</p>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
       <AppHeader>내 예약</AppHeader>
-
       <div className="p-6">
-        <div className="relative border rounded p-4 pt-3 bg-white">
-          <StateBadge state="proc" className="absolute top-3 right-3" />
-          <p className="font-bold text-xl mb-2">13가 1234</p>
-          <p className="text-sm font-semibold mt-2">차량</p>
-          <p>현대 / 소나타 19년식</p>
-          <p className="text-sm font-semibold mt-2">예약자</p>
-          <p>이용완 / 010-2222-3333</p>
-          <p className="text-sm font-semibold mt-2">예약내용</p>
-          <p>빨리 폐차하고 싶어요. 연락 빠르게 주세요.</p>
-        </div>
+        {result.data?.length > 0
+          ? result.data.map((o) =>
+            <div key={o.reservationId} className="relative border rounded p-4 pt-3 bg-white mt-5 first:mt-0">
+              <StateBadge state="proc" className="absolute top-3 right-3"/>
+              <p className="font-bold text-xl mb-2">{o.car.licensePlate}</p>
+              <p className="text-sm font-semibold mt-2">차량</p>
+              <p>{o.car.make} / {o.car.model}</p>
+              <p className="text-sm font-semibold mt-2">예약자</p>
+              <p>{o.clientName} / {toPhoneNoFormat(o.phoneNo)}</p>
+              <p className="text-sm font-semibold mt-2">예약내용</p>
+              <p>-</p>
+              <p className="text-sm font-semibold mt-2">견적 정보</p>
+              <ul>
+                {o.estimate?.length > 0
+                  ? o.estimate.map((eo, idx) =>
+                    <li
+                      key={idx}
+                      className={classNames(!eo.isFinal && 'line-through text-black/40')}
+                    >· {toAmountFormat(eo.amount)} / {eo.description}</li>
+                  )
+                  : <li>아직 등록된 견적이 없습니다.</li>
+                }
+              </ul>
+            </div>)
+          : <p className="text-center">등록된 예약 정보가 없습니다.</p>
+        }
       </div>
     </>
   );
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,10 +1,32 @@
 import { http, HttpResponse } from 'msw';
 
 export const handlers = [
-  http.get(process.env.BACKEND_API_URL + '/token/kakao', () => {
+  http.get(process.env.BACKEND_API_URL + '/reservation', () => {
     return HttpResponse.json({
-      clientId: process.env.MOCK_KAKAO_CLIENT_ID,
-      redirectUri: process.env.MOCK_KAKAO_REDIRECT_URI,
+      code: 'NORMAL',
+      message: '',
+      data: [{
+        reservationId: 'test-01',
+        userId: 'userId',
+        clientName: '홍길동',
+        phoneNo: '01011112222',
+        startTime: '2024-09-08T10:24:42.0059463',
+        endTime: '2024-09-09T10:24:42.0059463',
+        status: 'PENDING',
+        car: {
+          make: '현대',
+          model: '아반떼 19년식',
+          licensePlate: '12차 2345',
+        },
+        estimate: [{
+          amount: 1000.0,
+          issuedDate: '2024-09-08T10:24:42.0069468',
+          description: '이래저래 가격이럼',
+          isFinal: true,
+        }],
+        createdAt: '2024-09-08T10:24:42.0069468',
+        updatedAt: '2024-09-08T10:24:42.0069468'
+      }],
     });
   }),
 ];
