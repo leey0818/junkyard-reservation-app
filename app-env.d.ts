92ea3c6bcbf6b47b8166aae7ed5b34ccb5dc2128
leey0818 <leey0818@gmail.com>
2024-12-09 09:28:31 +0900

    chore: msw 패키지 삭제

--- app-env.d.ts
+++ app-env.d.ts
@@ -1,9 +1,6 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      MOCK_KAKAO_CLIENT_ID: string;
-      MOCK_KAKAO_REDIRECT_URI: string;
-
       BACKEND_API_URL: string;
       DATA_ENCRYPTION_KEY: string;
     }
--- src/instrumentation.ts
+++
@@ -1,6 +0,0 @@
-export async function register() {
-  if (process.env.NODE_ENV === 'development' && process.env.NEXT_RUNTIME === 'nodejs') {
-    const { server } = await import('@/mocks/server');
-    server.listen({ onUnhandledRequest: 'bypass' });
-  }
-}
